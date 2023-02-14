09fd76961f8cd8f3e17a280acf2ab35fa284d958
Yusuff Oyedele <oyedeleyusuff@gmail.com>
2023-02-14 20:57:26 +0100

    initial commit

---
+++ ActionApp/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+
+# Create your models here.
+
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveManager, self).get_queryset().filter(is_delete=False)
+
+
+class DeleteManager(models.Manager):
+    def get_queryset(self):
+        return super(DeleteManager, self).get_queryset().filter(is_delete=True)
+
+
+class Action(models.Model):
+    name = models.CharField(max_length=200)
+    decription = models.TextField()
+    schedule_time = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = models.Manager()
+    active_objects = ActiveManager()
+    deleted_objects = DeleteManager()
+
+    def __str__(self):
+        return self.name
+
+
+class Mail(Action):
+    subject = models.CharField(max_length=100)
+    attachment = models.FileField(null=True, blank=True, upload_to="attachment")
+    receiver_mail = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Reminder(Action):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
+class Message(Action):
+    phone_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Sender(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
